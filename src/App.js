cf3e8f0f0373ad8d57cdc2a63b321ac9d20dbc63
Muhammedalisert <140173103+Muhammedalisert@users.noreply.github.com>
2024-07-05 22:21:03 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo2 from './logo.svg';
-
-
+import {useEffect} from "react";
+import styles from './App.module.css';
+import Test from "./Test";
+import { Title } from "./Components";
 function App() {
+    useEffect(()=>{
+        console.log(styles.App)
+         if (process.env.NODE_ENV === 'production'){}
+
+         // analytici bilgilendir
+        },[])
   return (
     <div className="App">
-     <img src={logo2}/>
-      <img src="/logo512.png"/>
+        <Title>{process.env.NODE_ENV}</Title>
+        <p>
+            { process.env.REACT_APP_API_URL}
+        </p>
+        <Test/>
+        {process.env.NODE_ENV === 'production' && (
+            <div>
+                <img src={logo2} alt="ehgerh" />
+                <img src="/logo512.png " alt="nn"/>
+            </div>
+        )}
     </div>
   );
 }
---
+++ src/Test.js
@@ -0,0 +1,8 @@
+function Test(){
+    return(
+        <div>
+            test component
+        </div>
+    )
+}
+export default Test
